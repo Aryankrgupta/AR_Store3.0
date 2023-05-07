105dbaa6619bfaa2004960a5f8e951fe69311eda
Aryankrgupta <aryankumargupta1106@gmail.com>
2023-05-07 09:17:26 +0530

    five

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import AdminStatus from "@/components/AdminStats";
 import Layout from "@/components/layout";
-import LoginHeader from "@/components/LoginHeader";
+import LoginHeader from "@/components/loginHeader";
 
 export default function Home() {
   return <Layout>
