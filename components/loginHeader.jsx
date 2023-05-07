179969b26835654cbe80dba3b8796d96f819c5d9
Aryankrgupta <aryankumargupta1106@gmail.com>
2023-05-07 10:28:20 +0530

    nine

--- components/loginHeader.jsx
+++ components/loginHeader.jsx
@@ -1,7 +1,7 @@
 import { useSession } from 'next-auth/react'
 
 export default function LoginHeader() {
-  const [session] = useSession();
+  const { data: session } = useSession();
   return (
     <div className="text-blue-900 flex justify-between">
     <h2>
