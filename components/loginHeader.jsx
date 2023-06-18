9cd797b087bdb258641a62d620e959706d66958b
Aryan_ gupta <aryankumargupta1106@gmail.com>
2023-06-18 15:10:22 +0530

    loginheader

--- components/loginHeader.jsx
+++ components/loginHeader.jsx
@@ -1,23 +1,33 @@
-import { useSession } from 'next-auth/react'
+import { useSession } from 'next-auth/react';
 
-export default function loginHeader() {
-  const { data: session } = useSession();
+function LoginHeader() {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (!session) {
+    return <div>Not logged in</div>;
+  }
+
   return (
     <div className="text-blue-900 flex justify-between">
-    <h2>
-      <div className="flex gap-2 items-center">
-        <img src={session?.user?.image} alt="" className='w-6 h-6 rounded-md sm:hidden' />
-        <div>
-        Hello, <b>{session?.user?.name}</b>
+      <h2>
+        <div className="flex gap-2 items-center">
+          <img src={session.user.image} alt="" className="w-6 h-6 rounded-md sm:hidden" />
+          <div>
+            Hello, <b>{session.user.name}</b>
+          </div>
         </div>
+      </h2>
+      <div className="hidden sm:block flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session.user.image} alt="" className="w-6 h-6" />
+        <span className="px-2">{session.user.name}</span>
       </div>
-    </h2>
-    <div className='hidden sm:block flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden'>
-    <img src={session?.user?.image} alt="" className='w-6 h-6' />
-    <span className="px-2">
-    {session?.user?.name}
-    </span>
     </div>
-  </div>
-  )
+  );
 }
+
+export default LoginHeader;
+
